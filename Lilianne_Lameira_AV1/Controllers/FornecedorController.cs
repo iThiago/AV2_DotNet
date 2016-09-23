fed5c45da9f2bf4b1fe883940cc82c2bf1002064
thiago <thiagonetorj@hotmail.com>
2016-09-23 13:37:52 -0300

    fornecedorcntroller

---
+++ Lilianne_Lameira_AV1/Controllers/FornecedorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Application.Interfaces;
+using Domain.Entities;
+
+namespace Lilianne_Lameira_AV1.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        private readonly IFornecedorAppService _FornecedorApp;
+
+        public FornecedorController(IFornecedorAppService FornecedorApp)
+        {
+            _FornecedorApp = FornecedorApp;
+        }
+
+        [HttpPost][HttpGet]
+        public void SaveFornecedor(Fornecedor Fornecedor)
+        {
+            _FornecedorApp.Add(Fornecedor);
+        }
+
+        // GET: Fornecedor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
