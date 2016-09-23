8193de99c392eae29d3947a486b53f362c51c4c8
thiago <thiagonetorj@hotmail.com>
2016-09-23 13:47:18 -0300

    Fornecedor Controller TA INCOMPLETO

---
+++ Lilianne_Lameira_AV1/Scripts/App/fornecedor/controllers/FornecedorController.js
@@ -0,0 +1,33 @@
+﻿app.controller('FornecedorController', function ($scope, $http, $location) {
+    $scope.save = function () {
+        if ($scope.Cnpj === "") {
+            alert("CNPJ invalido.");
+            return;
+        }
+        if ($scope.RazaoSocial === "") {
+            alert("Razao Social invalida.");
+            return;
+        }
+
+        $http.post('/Fornecedor/SaveFornecedor', {
+            RazaoSocial: $scope.RazaoSocial,
+            Cpnj: $scope.Cpnj,
+            InscricaoMunicipal: $scope.InscricaoMunicipal,
+            ReceitaBruta: $scope.ReceitaBruta
+        }).then(function () {
+            $scope.name = "";
+            $scope.stock = "";
+            $scope.brand = "";
+            alert("Fornecedor salvo com sucesso.");
+        }, function (error) {
+            alert(error.Message);
+            console.log(error);
+        });
+    };
+
+    $scope.AddEndereco = function () {
+        //$scope.Enderecos.a
+    };
+
+
+});
